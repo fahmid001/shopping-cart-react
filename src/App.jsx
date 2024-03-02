83190889fa4b2aa02a7650ef48fbc7b74d927b86
Md. Fahmid Al Masud <fahmid789@gmail.com>
2024-03-02 17:36:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Welcome
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
